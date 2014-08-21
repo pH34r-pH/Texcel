ac0a212226686e81204300df299d59dc18053afe
pH34r-pH <purpleleven@gmail.com>
2014-08-21 08:45:00 -0400

    Adding excel ability

--- textToExcel.cs
+++ textToExcel.cs
@@ -1,20 +1,21 @@
- using System;
- using System.IO;
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Office.Interop.Excel;
 
-
-class rawParser
+public class rawParser
 {
-	public static void Main()
+	static void Main()
 	{
 		try{
 			using (StreamReader sr = new StreamReader("test.txt"))
 			{
-				string raw = sr.ReadAllText();
+				string raw = sr.ReadToEnd();
 				string[] scans = raw.Split(';');
 				foreach(string s in scans)
 				{
-   					// Consume
-	   				Console.WriteLine("We have {0}", s);
+					string scan = s.Trim();
+	   				Console.WriteLine("We have {0}", scan);
 				}
 			}
 		}
@@ -23,5 +24,33 @@ class rawParser
 			Console.WriteLine("File error: ");
 			Console.WriteLine(e.Message);
 		}
+		Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+		if(excel == null)
+		{
+			Console.WriteLine("Excel could not be started.");
+			return;
+		}
+		excel.Visible = true;
+
+		Workbook wb = excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+		Worksheet ws = (Worksheet)wb.Worksheets[1];
+
+		if( ws == null )
+		{
+			Console.Writeline("Worksheet could not be created.");
+		}
+
+		Range aRange = ws.get_Range("C1", "C7");
+
+		if(aRange == null)
+		{
+			Console.WriteLine("Could not get range.");
+		}
+
+		Object[] args = new Object[1];
+        args[0] = 6;
+        aRange.GetType().InvokeMember("Value", BindingFlags.SetProperty, null, aRange, args);
+    
+        aRange.Value2 = 8;
 	}
 }
