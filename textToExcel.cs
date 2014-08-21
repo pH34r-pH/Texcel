e2619f07832690eacf4344974f9bbbbe665c6ab1
pH34r-pH <purpleleven@gmail.com>
2014-08-21 07:38:08 -0400

    Initial commit

---
+++ textToExcel.cs
@@ -0,0 +1,27 @@
+ using System;
+ using System.IO;
+
+
+class rawParser
+{
+	public static void Main()
+	{
+		try{
+			using (StreamReader sr = new StreamReader("test.txt"))
+			{
+				string raw = sr.ReadAllText();
+				string[] scans = raw.Split(';');
+				foreach(string s in scans)
+				{
+   					// Consume
+	   				Console.WriteLine("We have {0}", s);
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			Console.WriteLine("File error: ");
+			Console.WriteLine(e.Message);
+		}
+	}
+}
